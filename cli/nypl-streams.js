25caba4763610aabc80c13c129d5ecd4555ef387
Paul Beaudoin <contact@paulbeaudoin.com>
2019-09-20 11:31:55 -0400

    Add simple cli for writing to streams

--- cli/nypl-streams.js
+++ cli/nypl-streams.js
@@ -1,8 +1,41 @@
 #!/usr/bin/env node
 
-const client = require('@nypl/nypl-streams-client')
+const dotenv = require('dotenv')
+const aws = require('aws-sdk')
+
+const Client = require('../index')
 const argv = require('minimist')(process.argv.slice(2))
 
-console.log('argv: ', client, argv)
+if (!argv.envfile) throw new Error('Must specify --envfile; See config/sample.env')
+
+if (!argv.profile) throw new Error('Must specify --profile')
+
+function setProfile (profile) {
+  // Set aws creds:
+  aws.config.credentials = new aws.SharedIniFileCredentials({
+    profile
+  })
+
+  // Set aws region:
+  let awsSecurity = { region: 'us-east-1' }
+  aws.config.update(awsSecurity)
+}
+
+function writeToStream(streamName, data) {
+  // Schema name is the stream name minus the env suffix:
+  schemaName = argv.schemaName || streamName.replace(/-.*/, '')
+
+  data = JSON.parse(data)
+
+  client.write (streamName, data, { avroSchemaName: schemaName })
+}
+
+dotenv.config({ path: argv.envfile })
+
+setProfile(argv.profile)
+
+const client = new Client({ nyplDataApiClientBase: process.env.NYPL_API_BASE_URL })
+
 switch (argv._[0]) {
+  case 'write': writeToStream(argv._[1], argv._[2])
 }
